5828350f0bb715f03a500eaa36dce394adc15a09
arezoofaraji <arezoo.faraji66@gmail.com>
2020-08-10 16:08:04 +0200

    11 Done

---
+++ arezoo/script.js
@@ -0,0 +1,9 @@
+let mystr = "It's hard to say who is right in this matter";
+let result0 = mystr.indexOf("a", 17);
+console.log(result0);
+let result1 = mystr.includes("righT");
+console.log(result1);
+let result2 = mystr.indexOf("who");
+console.log(result2);
+let result3 = mystr.charAt(mystr.length - 1);
+console.log(result3);
